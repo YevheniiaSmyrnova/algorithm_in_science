89eed67184083f74a1cf86cba23a817744bbb25b
YevheniiaSmyrnova <y.smyrnova@quantumobile.com>
2018-04-14 18:14:15 +0300

    make my example

--- 1_training_logistic_regression.py
+++ 1_training_logistic_regression.py
@@ -1,86 +1,72 @@
-from neo4j.v1 import GraphDatabase, basic_auth
+"""
+Training logistic regression
+"""
 import pandas as pd
-
+import random
+from neo4j.v1 import GraphDatabase, basic_auth
 from sklearn.linear_model import LogisticRegression
 
-# ----- connection -------
-
+# Connect to database
 db_location = "bolt://0.0.0.0:7687"
 username = "neo4j"
 password = "12345qwert"
 driver = GraphDatabase.driver(db_location, auth=basic_auth(username, password))
 
+# Work with database
 with driver.session() as session:
-    # erase database
     with session.begin_transaction() as clear_db:
         clear_db.run("MATCH (n) DETACH DELETE n")
 
-    # add users with properties score_math up to 10 and score_English up to 10
-    with session.begin_transaction() as create_users:
-        create_users.run("FOREACH(r IN range(1,10)| "
-                         "CREATE (u :User {user_id: r, score_math: rand()*10, score_English: rand()*10}))")
+    with session.begin_transaction() as create_developers:
+        create_developers.run(
+            "FOREACH(r IN range(1,10)| "
+            "CREATE (u :Developer {developer_id: r, django_knowledge: rand()*10, angular_knowledge: rand()*10}))")
 
-        # add random relationships between users
     with session.begin_transaction() as add_relationships:
-        add_relationships.run("MATCH(user_1 :User), (user_2 :User) "
-                              "WHERE rand()<0.3 AND NOT user_1.user_id=user_2.user_id "
-                              "CREATE(user_1)-[:INTERESTED_IN]->(user_2)")
-with driver.session() as session:
-    # get vectors with data from database
+        add_relationships.run(
+            "MATCH(developer_1 :Developer), (developer_2 :Developer) "
+            "WHERE rand()<0.3 AND NOT developer_1.developer_id=developer_2.developer_id "
+            "CREATE(developer_1)-[:NEEDS_HELD]->(developer_2)")
+
     with session.begin_transaction() as get_data:
         result = get_data.run(
-            "MATCH(user_1 :User), (user_2 :User) "
-            "WHERE NOT user_1.user_id=user_2.user_id "
-            "WITH user_1.user_id as interested_user_id, user_2.user_id as user_id, "
-            "user_1.score_math as interested_score_math, user_2.score_math as user_score_math, "
-            "user_1.score_English as interested_score_English, user_2.score_English as user_score_English, "
-            "CASE WHEN (user_1)-[:INTERESTED_IN]->(user_2) THEN 1 ELSE 0 END as interested_in "
-            "RETURN interested_user_id, user_id, interested_score_math, user_score_math, interested_score_English, user_score_English, interested_in "
+            "MATCH(developer_1 :Developer), (developer_2 :Developer) "
+            "WHERE NOT developer_1.developer_id=developer_2.developer_id "
+            "WITH developer_1.developer_id as developer_needs_help, "
+            "developer_2.developer_id as developer, "
+            "developer_1.django_knowledge as needed_django_knowledge, "
+            "developer_2.django_knowledge as django_knowledge, "
+            "developer_1.angular_knowledge as needed_angular_knowledge, "
+            "developer_2.angular_knowledge as angular_knowledge, "
+            "CASE WHEN (developer_1)-[:NEEDS_HELD]->(developer_2) THEN 1 ELSE 0 END as needs_help "
+            "RETURN developer_needs_help, developer, needed_django_knowledge, django_knowledge, "
+            "needed_angular_knowledge, angular_knowledge, needs_help "
         )
         training_data = pd.DataFrame([{k: v for k, v in r.items()} for r in result])
-
-print training_data
+print "Test data set information"
 print training_data.info()
 
-# create 2 variables called X_data and Y_data:
-# X_data shall be a matrix with features columns
-# and Y_data shall be a matrix with responses columns
-
-features_columns = [x for x in training_data.columns if x not in ['interested_user_id','user_id', 'interested_in']]
-responses_columns = ['interested_in']
+# Make test data set
+# X_data - matrix with features columns
+# Y_data - matrix with responses columns
+features_columns = [x for x in training_data.columns if x not in ['developer_needs_help', 'developer', 'needs_help']]
+responses_columns = ['needs_help']
 X_data = training_data[features_columns]
 Y_data = training_data[responses_columns]
 
-print X_data.head()
+y = Y_data["needs_help"].values
 
-print Y_data
+# Work with sklearn
+model = LogisticRegression()
+model.fit(X_data, y)
 
-y = Y_data["interested_in"].values
-
-print y
-
-# import Logistic Regression
-
-# give a name to Logistic regression model
-model_1 = LogisticRegression()
-
-# put our data to train model
-model_1.fit(X_data, y)
-
-# create new data
-
-new_data = pd.DataFrame(data = {'interested_score_English': [9.3, 8.2, 0.1], 'interested_score_math': [0.45, 4.6, 8.3], 'user_score_English': [2.7, 4.2, 5.3], 'user_score_math': [7.4, 6.5, 5.4]})
-
-print new_data
-
-# predict a value of getting values 0 or 1
-print model_1.predict(new_data)
-
-# predict probability of getting value 1 (we will have probabilities of getting 0 an 1, we need 1)
-print model_1.predict_proba(new_data)[:, 1]
-
-output = model_1.predict_proba(new_data)[:, 1]
-
-output_data = pd.DataFrame({'interested_in': output})
-
+# New data set and predict
+new_data = pd.DataFrame(data={'needed_django_knowledge': [random.randint(0, 10), random.randint(0, 10), random.randint(0, 10)],
+                              'needed_angular_knowledge': [random.randint(0, 10), random.randint(0, 10), random.randint(0, 10)],
+                              'django_knowledge': [random.randint(0, 10), random.randint(0, 10), random.randint(0, 10)],
+                              'angular_knowledge': [random.randint(0, 10), random.randint(0, 10), random.randint(0, 10)]})
+model.predict(new_data)
+output = model.predict_proba(new_data)[:, 1]
+output_data = pd.DataFrame({'needs_help': output})
+print "Our prediction:"
 print output_data

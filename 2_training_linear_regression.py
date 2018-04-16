7e492e4654232f7a80bcbb6324a57448544bdc6a
YevheniiaSmyrnova <y.smyrnova@quantumobile.com>
2018-04-16 16:04:52 +0300

    clean up

--- 2_training_linear_regression.py
+++ 2_training_linear_regression.py
@@ -53,55 +53,6 @@ with driver.session() as session:
                 RETURN customer_id, name, income, cooperation_duration, transaction""")
             training_data = pd.DataFrame([{k: v for k, v in r.items()} for r in result])
 
-    # # create year
-    # with session.begin_transaction() as create_year:
-    #     create_year.run("CREATE (year :Year) set year.year=2018")
-    #
-    #     # create months
-    # with session.begin_transaction() as create_months:
-    #     create_months.run(
-    #         """MATCH (year :Year)
-    #         WHERE year.year = 2018
-    #         FOREACH (month IN range (4,12) | MERGE (:Month {month: month, year: 2018})-[:BELONGS_TO]->(year))""")
-    #
-    # # create days in April
-    # with session.begin_transaction() as create_days:
-    #     create_days.run(
-    #         """MATCH (month :Month)
-    #         WHERE month.month = 4 AND month.year = 2018
-    #         FOREACH(day IN range (1,30) | MERGE (:Day {day: day, month: 4, year: 2018})-[:BELONGS_TO]->(month))""")
-    #
-    # # create sportsmans
-    # with session.begin_transaction() as create_sportsmans:
-    #     create_sportsmans.run(
-    #         """WITH ["Anna", "Tom", "Nick", "Kateryna", "Maryna", "Jay"] AS name
-    #         FOREACH (i IN range(1,24) |
-    #         CREATE (s :Sportsman {sportsmen_id: i, FullName: name[i % size(name)], age: round(rand()*34)+14, duration_training: round(rand()*8)+1}))""")
-    #
-    # for i in range(1, 84):
-    #     # create training
-    #     with session.begin_transaction() as create_training:
-    #         create_training.run(
-    #             """WITH round(rand()*23+1) as sportsmen_id, round(rand()*29+1) as Day, 4 as Month, 2018 as Year
-    #             MATCH (sportsman :Sportsman), (year :Year), (month :Month), (day :Day)
-    #             WHERE sportsman.sportsmen_id = sportsmen_id
-    #             AND year.year=Year AND month.month=Month AND month.year=Year
-    #             AND day.day=Day AND day.month=Month AND day.year=Year
-    #             MERGE(sportsman)-[:HAS_TRAINING]->(day)"""
-    #             )
-    #
-    # # get data from database
-    # with session.begin_transaction() as get_data:
-    #     result = get_data.run(
-    #         """MATCH(sportsman :Sportsman)-[ht :HAS_TRAINING]-(day :Day)-[:BELONGS_TO]->(:Month)-[:BELONGS_TO]->(:Year)
-    #         WITH sportsman.sportsman_id as sportsman_id,
-    #         sportsman.FullName as name,
-    #         sportsman.age as age,
-    #         sportsman.duration_training as duration_training,
-    #         COUNT(ht) as training
-    #         RETURN sportsman_id, name, age, duration_training, training""")
-    #     training_data = pd.DataFrame([{k: v for k, v in r.items()} for r in result])
-
 print "Test data set information:"
 print training_data.info()
 
